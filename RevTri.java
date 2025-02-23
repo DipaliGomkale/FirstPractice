70d92fcd625f7dc0e44f71bcb5f595b3509f53ac
Dipali Gomkale <dipaligomkale256@gmail.com>
2025-02-23 19:16:20 +0530

    Changes done

---
+++ RevTri.java
@@ -0,0 +1,14 @@
+public class RevTri
+{
+	public static void main(String x[])
+	{
+	for(int i=4;i>=1;i--)
+	{
+	for(int j=1;j<=i;j++)
+	{
+		System.out.print(i);
+	}
+		System.out.println();
+	}
+	}
+}
